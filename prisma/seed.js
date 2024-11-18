41358be3415d4b065f48aac03ea367ce47fb6f90
Scott <Scottyjmt13@gmail.com>
2024-11-18 11:10:53 -0700

    practice complete

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,28 @@
 const prisma = require("../prisma");
-const seed = async () => {
-  // TODO: Create 3 restaurants with 5 reservations each
+const { faker } = require("@faker-js/faker");
+
+const seed = async (numRestaurants = 3, numReservations = 5) => {
+  for (let i = 0; i < numRestaurants; i++) {
+    const reservations = Array.from({ length: numReservations }, (_, j) => {
+      const name = faker.internet.displayName();
+      return {
+      name: `Person ${i}${j}`,
+      email: `${i}${j}@yahoo.com`,
+      partySize: Math.floor(Math.random() * 10) + 1,
+      };
+    });
+
+    await prisma.restaurant.create({
+      data: {
+        name: faker.company.buzzAdjective() + " " + faker.company.buzzNoun(),
+        reservations: {
+          create: reservations,
+        }
+      }
+    });
+  }
 };
+
 seed()
   .then(async () => await prisma.$disconnect())
   .catch(async (e) => {
